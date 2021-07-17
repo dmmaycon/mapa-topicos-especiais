ec61fd392911c3eecd0884741bd3e3d1726a33ea
Maycon de Moraes <mayconmoraes.dm@gmail.com>
2021-07-17 00:43:10 -0300

    Melhorias finalizadas para entrega MAPA

--- src/dmmaycon/github/com/topicos/especiais/mapa/Data.java
+++ src/dmmaycon/github/com/topicos/especiais/mapa/Data.java
@@ -7,7 +7,7 @@ public class Data {
 	private Integer month;
 
 	private Integer year;
-	
+
 	private Boolean valid = false;
 
 	public Data(Integer day, Integer month, Integer year) throws Exception {
@@ -21,88 +21,92 @@ public class Data {
 		}
 
 	}
-	
+
 	public boolean isValid() {
 		return this.valid;
 	}
 
-	public boolean validarData(Integer day, Integer month, Integer year) throws Exception {
-
-		if ((month < 1 || month > 12) || (day < 1 || day > 31) || (year <= 0)) {
-
-			throw new Exception("Valores para dia, mês ou ano são inválidos.");
-
-		} else {
-
-			boolean flag = false;
-
-			if (month == 4 || month == 6 || month == 9 || month == 11) {
-
-				if (day <= 30) {
-
-					flag = true;
-
-				} else
-					flag = false;
-
+	public Data diaAnterior() throws Exception {
+		try {
+			Data diaAnterior = new Data(day - 1, month, year);
+			if (diaAnterior.isValid()) {
+				return diaAnterior;
 			}
-
-			if (month == 1 || month == 3 || month == 5 || month == 7 ||
-
-					month == 8 || month == 10 || month == 12) {
-
-				if (day <= 31) {
-
-					flag = true;
-
-				} else {
-
-					flag = false;
-
-				}
-
-			}
-
-			if (month == 2) {
-
-				if (year % 4 == 0) {
-
-					if (day <= 29) {
-
-						flag = true;
-
-					} else {
-
-						flag = false;
-
-					}
-
-				} else {
-
-					if (day <= 28) {
-
-						flag = true;
-
-					} else {
-
-						flag = false;
-
-					}
-
-				}
-
-			}
-
-			if (flag) {
-
-				return true;
-
-			} else {
-
-				return false;
-
-			}
+		} catch (Exception e) {
 
 		}
+		return diaAnteriorMudandoMesOuAno();
+	}
+
+	private Data diaAnteriorMudandoMesOuAno() throws Exception {
+		if (validaMesTrintaEUm(month, day)) {
+			// janeiro
+			if (month == 1) {
+				return new Data(31, 12, year - 1);
+			}
+			// marco
+			if (month == 3) {
+				if (validaMesFevereiro(2, 29, year)) {
+					return new Data(29, 2, year);
+				}
+				return new Data(28, 2, year);
+			}
+			return new Data(30, month - 1, year);
+		}
+
+		if (validaMesTrinta(month, day)) {
+			return new Data(31, month - 1, year);
+		}
+		return null;
+	}
+
+	private boolean validarData(Integer day, Integer month, Integer year) throws Exception {
+
+		if ((month < 1 || month > 12) || (day < 1 || day > 31) || (year <= 0)) {
+			throw new Exception("Valores para dia, mês ou ano são inválidos.");
+		}
+
+		if (validaMesTrintaEUm(month, day) || validaMesTrinta(month, day) || validaMesFevereiro(month, day, year)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean validaMesTrinta(Integer month, Integer day) {
+		if (month == 4 || month == 6 || month == 9 || month == 11) {
+			if (day <= 30) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean validaMesTrintaEUm(Integer month, Integer day) {
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			if (day <= 31) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean validaMesFevereiro(Integer month, Integer day, Integer year) {
+		if (month == 2) {
+			if (year % 4 == 0) {
+				if (day <= 29) {
+					return true;
+				}
+			} else {
+				if (day <= 28) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return day + "/" + month + "/" + year;
 	}
 }
--- src/dmmaycon/github/com/topicos/especiais/mapa/tests/DataTest.java
+++ src/dmmaycon/github/com/topicos/especiais/mapa/tests/DataTest.java
@@ -11,7 +11,7 @@ class DataTest {
 	@Test
 	void testaDataDiaInvalido() {
 		try {
-			new Data(32, 01, 2021);
+			new Data(0, 01, 2021);
 			fail("Dia invalido aceito");
 		} catch (Exception e) {
 			assertEquals(e.getMessage(),"Valores para dia, mês ou ano são inválidos.");
@@ -42,6 +42,43 @@ class DataTest {
 	void testaDataValida() throws Exception {
 		Data data = new Data(14, 06, 2021);
 		assertEquals(data.isValid(), true);	
+	}
+	
+	@Test
+	void testaDiaAnteriorMeioMes() throws Exception {
+		Data data = new Data(14, 6, 2021);
+		assertEquals(data.diaAnterior().toString(), "13/6/2021");	
+	}
+
+	
+	@Test
+	void testaDiaAnteriorInicioMesTrintaEUmDias() throws Exception {
+		Data data = new Data(1, 6, 2021);
+		assertEquals(data.diaAnterior().toString(), "31/5/2021");	
+	}
+	
+	@Test
+	void testaDiaAnteriorInicioMesTrintaDias() throws Exception {
+		Data data = new Data(1, 5, 2021);
+		assertEquals(data.diaAnterior().toString(), "30/4/2021");	
+	}
+	
+	@Test
+	void testaDiaAnteriorFevereiro() throws Exception {
+		Data data = new Data(1, 3, 2021);
+		assertEquals(data.diaAnterior().toString(), "28/2/2021");	
+	}
+	
+	@Test
+	void testaDiaAnteriorFevereiroVinteOito() throws Exception {
+		Data data = new Data(1, 3, 2020);
+		assertEquals(data.diaAnterior().toString(), "29/2/2021");	
+	}
+	
+	@Test
+	void testaDiaAnteriorViradaAno() throws Exception {
+		Data data = new Data(1, 1, 2020);
+		assertEquals(data.diaAnterior().toString(), "31/12/2019");	
 	}
 
 }
